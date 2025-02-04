ee0bc376a424e0268475735d5ef4cb7396c00d1f
“Anastasiia <antariys081@gmail.com>
2025-02-04 21:14:50 +0200

    hw3

---
+++ Homework/src/hw2/Main.java
@@ -0,0 +1,26 @@
+package hw2;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int orderNo=1;
+        String client="Alice.";
+        String product="smartphone,";
+        double price= 305.99d;
+        String address="Moon Street, 10.";
+        System.out.print("Order No "); System.out.print(orderNo); System.out.print(" Client: "); System.out.println(client);
+        System.out.print("Product: "); System.out.println(product);
+        System.out.print("price EUR "); System.out.print(price); System.out.println(".");
+        System.out.print("Address: "); System.out.println(address);
+        orderNo=2;
+        client="Tom.";
+        product="laptop,";
+        price= 570.95d;
+        address="Terra Street, 17.";
+        System.out.print("Order No "); System.out.print(orderNo); System.out.print(" Client: "); System.out.println(client);
+        System.out.print("Product: "); System.out.println(product);
+        System.out.print("price EUR "); System.out.print(price); System.out.println(".");
+        System.out.print("Address: "); System.out.print(address);
+
+    }
+}
